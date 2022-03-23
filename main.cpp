9382753f3079b39401c1859e52e649adab54ad41
simon <simon-hiller@gmx.net>
2022-03-23 20:41:52 +0100

    Joa

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+
+    int a = 4;
+    std::cout << "Hello, World!" << std::endl;
+    cout << a << endl;
+
+    return 0;
+}
